e094426d5b6ac364ac2cfe4b3ead1a47501b15b5
GS_dev <vitealonso59@gmail.com>
2024-02-29 19:25:29 -0500

    Proyecto Agregado

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('passwordInput');
+    const showPasswordCheckbox = document.getElementById('showPassword');
+
+    showPasswordCheckbox.addEventListener('change', function() {
+        passwordInput.type = this.checked ? 'text' : 'password';
+    });
+});
